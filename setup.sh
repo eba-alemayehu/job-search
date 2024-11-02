993e7adc81ae0de0ca683be236511a354ab14b9e
eba <ebaalemayhu3@gmail.com>
2024-11-02 22:25:23 +0300

    docker to ubuntu

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pip install gunicorn --break-system-packages
-python manage.py migrate --no-input
-python manage.py collectstatic --noinput
-python manage.py crontab add
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --noinput
+python3 manage.py crontab add
 pwd
 ls
 gunicorn job.wsgi --bind 0.0.0.0:8000
