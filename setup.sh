ceb95db1fb18c263d2bcb5fa55220bff29b694e7
eba <ebaalemayhu3@gmail.com>
2024-11-02 21:01:54 +0300

    docker to ubuntu

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install gunicorn
+pip install gunicorn --break-system-packages
 python manage.py migrate --no-input
 python manage.py collectstatic --noinput
 python manage.py crontab add
